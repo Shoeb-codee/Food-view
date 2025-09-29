e3b2d2de15fd46a5f09cc0a9f34aa358b6cc0f5d
Shoeb-codee <shoebahmad698@gmail.com>
2025-09-29 13:32:28 +0530

    feat: integrate ImageKit for file uploads and add multer for handling multipart/form-data
    - Added ImageKit service for file uploads to cloud storage.
    - Implemented multer middleware to handle file uploads in food routes.
    - Updated food controller to utilize the new storage service for uploading files.
    - Added new dependencies: cloudinary, imagekit, multer, and uuid.
    - Created .gitignore file to exclude node_modules and .env files.

--- Backend/src/controllers/food.controller.js
+++ Backend/src/controllers/food.controller.js
@@ -1,10 +1,17 @@
-const foodmodel = require('../model/food.model')
-const { create } = require('../model/foodpartner.model')
+const foodmodel = require("../model/food.model");
+const storageService = require("../services/storage.service");
+const { v4: uuid } = require("uuid");
 
-const createFood = async (req, res) =>{
-res.send("Foood Item Created")
-}
+const createFood = async (req, res) => {
+  const fileUploadResult = await storageService.uploadFile(
+    req.file.buffer,
+    uuid()
+  );
+  console.log(fileUploadResult);
+
+  res.send("YES YES YES");
+};
 
 module.exports = {
-  createFood
-}
+  createFood,
+};
--- Backend/src/routes/food.routes.js
+++ Backend/src/routes/food.routes.js
@@ -2,9 +2,14 @@ const express = require('express');
 const authMiddleware = require('../middleware/auth.middleware');
 const router = express.Router();
 const foodController = require('../controllers/food.controller')
+const multer = require('multer');
+
+const upload = multer({
+  storage: multer.memoryStorage()
+})
 
 
-router.post('/create-food',authMiddleware.authFoodPartnerMiddleware,foodController.createFood)
+router.post('/create-food',authMiddleware.authFoodPartnerMiddleware,upload.single("video") ,foodController.createFood)
 
 
 module.exports = router;
